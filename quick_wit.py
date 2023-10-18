c02fcee752a359317b2a3594957a1904872fb187
Ilia Kaziamov <kaziamov@outlook.com>
2023-10-18 22:59:12 +0300

    Add Librarian()

--- quick_wit.py
+++ quick_wit.py
@@ -4,6 +4,7 @@ from tkinter import ttk, filedialog, messagebox
 import time
 import fitz  # PyMuPDF
 from PIL import Image, ImageTk
+import hashlib
 
 class PDFSpeedReaderApp:
     def __init__(self, root):
@@ -15,8 +16,6 @@ class PDFSpeedReaderApp:
         self.icon = ImageTk.PhotoImage(icon_image)
         self.root.iconphoto(True, self.icon)
 
-        self.history = {}
-        self.load_history()
 
         self.current_page_index = 0
         self.current_book = ""
@@ -24,6 +23,9 @@ class PDFSpeedReaderApp:
         self.words = []
         self.pages = []
         self.word_index = 0
+        self.history = {}
+        self.libratanian = Librarian()
+
         self.speed = 1100  # Default speed: Words per minute
         self.paused = False
 
@@ -45,40 +47,28 @@ class PDFSpeedReaderApp:
         self.book_name.insert(0, "Book Name")
 
         # Start button to begin reading
-        self.start_button = ttk.Button(root, text="Start", command=self.start_reading)
+        self.start_button = ttk.Button(root, text="Start reading", command=self.start_reading)
         self.start_button.pack(pady=10)
 
         # Stop button to pause reading
-        self.stop_button = ttk.Button(root, text="Stop", command=self.stop_reading)
+        self.stop_button = ttk.Button(root, text="Pause", command=self.stop_reading)
         self.stop_button.pack(pady=10)
         self.stop_button.config(state=tk.DISABLED)
 
         # Resume button to resume reading after stopping
-        self.resume_button = ttk.Button(root, text="Resume", command=self.resume_reading)
+        self.resume_button = ttk.Button(root, text="Continue", command=self.resume_reading)
         self.resume_button.pack(pady=10)
         self.resume_button.config(state=tk.DISABLED)
 
         # Previous button to go back reading after stopping
-        self.previous_button = ttk.Button(root, text="Previous", command=self.previous_reading)
+        self.previous_button = ttk.Button(root, text="Previous word", command=self.previous_reading)
         self.previous_button.pack(pady=10)
         self.previous_button.config(state=tk.DISABLED)
 
         # Next button to go next reading after stopping
-        self.next_button = ttk.Button(root, text="Next", command=self.next_reading)
+        self.next_button = ttk.Button(root, text="Next word", command=self.next_reading)
         self.next_button.pack(pady=10)
         self.next_button.config(state=tk.DISABLED)
-
-        # self.curent_page = ttk.Entry(root, text="Page:")
-        # self.curent_page.pack(pady=10)
-
-        # self.previous_page = ttk.Button(root, text="Previous Page", command=self.previous_reading)
-        # self.previous_page.pack(pady=10)
-        # self.previous_page.config(state=tk.DISABLED)
-
-        # self.next_page = ttk.Button(root, text="Next Page", command=self.next_reading)
-        # self.next_page.pack(pady=10)
-        # self.next_page.config(state=tk.DISABLED)
-
 
         # Speed control (WPM)
         self.speed_label = ttk.Label(root, text="Speed (WPM):")
@@ -92,63 +82,14 @@ class PDFSpeedReaderApp:
         self.speed_scale.set(self.speed)
         self.speed_scale.pack()
 
-
-    def load_history(self):
-        with open("history.json", "r") as f:
-            new_data = json.load(f)
-        self.history.update(new_data)
-
-
-    def save_history(self):
-        self.history[self.pdf_path] = (self.current_page_index, self.word_index)
-        with open("history.json", "w") as f:
-            json.dump(self.history, f, indent=4)
-
-
-    def save_book(self):
-        with open(str(self.book_name), "w", encoding="utf-8") as f:
-            f.writelines(self.pages)
-
-
     def load_pdf(self):
-        """Load a PDF file using the file dialog."""
         self.pdf_path = filedialog.askopenfilename(filetypes=[("PDF Files", "*.pdf")])
         if self.pdf_path:
-            self.pages = self.extract_text_from_pdf()
-            self.word_index = 0
-
-    def get_clear_words(self, text):
-        symbols = ".,!?:;"
-        clean_words = []
-        clean_word = ""
-        for word in text.split():
-            for char in word:
-                if char.isalpha() or char.isdigit():
-                    clean_word += char
-                elif char in symbols:
-                    clean_words.append("".join(clean_word))
-                    clean_word = char
-            clean_words.append("".join(clean_word))
-            clean_word = ""
-
-        return clean_words
-
-
-    def extract_text_from_pdf(self):
-        """Extract text from a PDF file using PyMuPDF."""
-        doc = fitz.open(self.pdf_path)
-        pages = []
-        for page in doc:
-            page_text = page.get_text()
-            new_words = self.get_clear_words(page_text)
-            if new_words:
-                pages.append(new_words)
-        return pages
+            self.pages = self.libratanian.get_book(self.pdf_path)
 
     def stop_reading(self):
         """Pause the reading process."""
         self.paused = True
-        self.save_history()
         self.stop_button.config(state=tk.DISABLED)
         self.resume_button.config(state=tk.NORMAL)
         self.previous_button.config(state=tk.NORMAL)
@@ -216,13 +157,8 @@ class PDFSpeedReaderApp:
         # Update the GUI
         self.root.update()
 
-
     def get_words(self):
         return self.pages[self.current_page_index]
-
-    def is_continue_reading(self):
-        return self.current_page_index < len(self.pages) and not self.paused
-
 
     def start_reading(self):
         """Start the reading process."""
@@ -263,6 +199,7 @@ class PDFSpeedReaderApp:
             if self.current_page_index == len(self.pages) - 1:
                 delay *= 3  # Display the last word for a longer time
 
+            self.history[self.pdf_path] = (self.current_page_index, self.word_index)
             time.sleep(delay)
 
         if self.word_index == len(self.words):
@@ -278,6 +215,89 @@ class PDFSpeedReaderApp:
         """Update the reading speed (WPM) and display the current WPM value."""
         self.speed = int(float(value))
         self.wpm_label.config(text=f"{self.speed} WPM")
+
+
+class Librarian:
+
+    def _get_hash_for_book(self, book):
+        return hashlib.md5(book.encode()).hexdigest()
+
+    def get_reading_history(self):
+        pass
+
+    def save_reading_history(self):
+        pass
+
+
+
+    def save_book(self, book_name, pages):
+        with open(f"{book_name}.rsvp", "w", encoding="utf-8") as f:
+            f.writelines(pages)
+
+    def load_book(self, book_name):
+        pass
+
+    def _load_book(self, pdf_path):
+        """Load a PDF file using the file dialog."""
+        doc = fitz.open(pdf_path)
+        pages = self._get_clear_pages(doc)
+        return pages
+
+    def _get_clear_words(self, text):
+        symbols = ".,!?:;"
+        clean_words = []
+        clean_word = ""
+        for word in text.split():
+            for char in word:
+                if char.isalpha() or char.isdigit():
+                    clean_word += char
+                elif char in symbols:
+                    clean_words.append("".join(clean_word))
+                    clean_word = char
+            clean_words.append("".join(clean_word))
+            clean_word = ""
+
+        return clean_words
+
+
+    def _get_clear_pages(self, doc):
+        """Extract text from a PDF file using PyMuPDF."""
+        pages = []
+        for page in doc:
+            text = ""
+            text += page.get_text()
+            new_words = self._get_clear_words(text)
+            if new_words:
+                pages.append(new_words)
+        return pages
+
+    def open_book(self):
+        pass
+
+    def get_book(self, path):
+        return self._load_book(path)
+
+
+    def save(self):
+        pass
+
+class BooksHistory:
+
+    def _load_history(self):
+        with open("history.json", "r") as f:
+            new_data = json.load(f)
+        self.history.update(new_data)
+
+    def _save_history(self, history):
+        with open("history.json", "w", encoding="utf-8") as f:
+            json.dump(history, f, indent=4)
+    def save(self, dict):
+        pass
+
+    def load(self):
+        return dict
+
+
 
 if __name__ == "__main__":
     root = tk.Tk()
